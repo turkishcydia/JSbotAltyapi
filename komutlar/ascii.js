388ac06ead08138270db22ebc0c7148fc3dbf105
__UwU__ <82547225+turkishcydia@users.noreply.github.com>
2021-10-18 13:51:50 +0300

    Update ascii.js

--- komutlar/ascii.js
+++ komutlar/ascii.js
@@ -1,25 +1,28 @@
+// Gerekli Modülleri Tanımlar.
 var figlet = require('figlet');
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args, tools) => {
-  var sınır = 75 // Kendiniz en yüksek harf sayısını ayarlayabilirsiniz
+  var sınır = 75 //ASCII Karakter Sınırını Belirler. Bende ayarını değiştirmeyin çünkü küçük ekranlarda sıkıntı çıkarabilir.
   
   if(args.join(' ').length > sınır) return message.channel.send(`Çok karakter yazdınız. En fazla ${sınır} karakter yazabilirsin!`) 
-     if(!args[0]) return message.channel.send('Lütfen geçerli yazı giriniz.');
+     if(!args[0]) return message.channel.send('Lütfen geçerli yazı giriniz.'); 
   
   figlet(`${args.join(' ')}`, function(err, data) {
       if (err) {
-          console.log('Bir hata var...');
+          console.log('Bir hata var...'); //Konsola Hata Mesajı Bildirir
           console.dir(err);
           return;
       }
 
-      message.channel.send(`${data}`, {code: 'AsciiArt'});
+      message.channel.send(`${data}`, {code: 'AsciiArt'}); //Yazının ASCII Halini Gönderir
 
   });
 
 };
 
+
+//Yardım Menüsünde Gösterme
 exports.conf = {
   aliases: [],
   permLevel: 0,
